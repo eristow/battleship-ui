c1c6d710006f0cdea73a0bc0baed8a1b2470dc8f
Evan Ristow <evan.ristow1@gmail.com>
2024-12-15 12:17:05 -0600

    Created initial connection to backend.

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# TODOs
+- [ ] Create user sign-up form
+- [ ] Create user login form
+- [ ] Create game creation page
+- [ ] Create game join page
+- [ ] Create make move page
+- [ ] Create game management page (CRUD logged in user's games)
+
+# DONE
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,31 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	/*
+	export interface ShipSummary {
+		name: ShipType;
+		length: number;
+		startX?: number;
+		startY?: number;
+		isHorizontal?: boolean;
+		currentHits?: number;
+	}
+	*/
+	let backend_url = import.meta.env.VITE_BACKEND_URL;
+	let ships = fetchShipTypes();
+
+	async function fetchShipTypes() {
+		const response = await fetch(`${backend_url}/games/ships`);
+		return await response.json();
+	}
+</script>
+
+<div class="m-2">
+	{#await ships}
+		<p>Loading...</p>
+	{:then response}
+		{#each response as ship}
+			<p>{ship.name}: length of {ship.length}</p>
+		{/each}
+	{:catch error}
+		<p>{error}</p>
+	{/await}
+</div>
